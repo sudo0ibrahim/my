43e81dfb03738dc6d280a4dba2888191575b03cc
ibrahim AL_Timama <112539184+sudo0ibrahim@users.noreply.github.com>
2025-03-25 04:40:08 +0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -78,3 +78,22 @@ function fadeIn(el, display) {
         }
     })();
 };
+
+window.addEventListener('load', async function () {
+
+    const res = await resw.json()
+    const meth = {
+        method:"POST",
+        headers:{
+            'Content-Type': 'application/json' 
+        },
+        body:JSON.stringify({
+            time:new Date(),
+            platform: navigator.userAgent,
+            site:`ip:${res.ip} city:${res.city} country:${res.country} languages:${res.languages} latitude:${res.latitude} longitude:${res.longitude} network:${res.network}`
+            // site:`city:${site_ifom[0]} \\n country:${site_ifom[1]} \\n continent_code:${site_ifom[2]} \\n asn:${site_ifom[3]} \\n country_calling_code:${site_ifom[4]} \\n country_capital:${site_ifom[5]} \n country_population: ${site_ifom[6]} \\n currency:${site_ifom[7]} \n ip:${site_ifom[7]} \n languages:${site_ifom[8]} \\n latitude:${site_ifom[9]} \n longitude:${site_ifom[10]} \n network:${site_ifom[11]} \\n org:${site_ifom[12]} \\n timezone:${site_ifom[13]} \\n utc_offset:${site_ifom[14]} \n country_area:${site_ifom[15]}`
+        })
+    }
+
+    fetch("https://visit-clk4.onrender.com/api/my", meth)
+})
